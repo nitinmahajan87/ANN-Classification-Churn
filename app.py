5898db85bcb7d677a4685cce68341eca5ca6531c
nitinmahajan87 <166050579+nitinmahajan87@users.noreply.github.com>
2025-07-20 23:36:06 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -16,12 +16,12 @@ with open('onehot_encoder_geo.pkl','rb') as file:
 with open('label_encoder_gender.pkl','rb') as file:
     label_encoder_gender = pickle.load(file)
 
-with open('scaler.pkl','rb') as file:
+with open('churn_scaler.pkl','rb') as file:
     scaler = pickle.load(file)
 
 ## Streamlit app
 
-st.title('Customer Churn Prediction')
+st.title('Custoomer Churn Prediction')
 
 #User Input
 geography = st.selectbox('Geography', label_encoder_geo.categories_[0])
